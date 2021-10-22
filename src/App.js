66bcdc81dc886968d52840b4cd46ec722f8ec896
Enoch2k2 <enoch2k2@hotmail.com>
2021-10-22 14:14:39 -0500

    added links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <div className="App">
         <h1>Hello World</h1>
       </div>
